a79aaf2d62d01bb72d42a976852041a5bf2f6416
Zacchary Favere <zfavere@c1r8p3.42nice.fr>
2024-02-10 18:32:05 +0100

    add

--- parsing.c
+++ parsing.c
@@ -37,17 +37,3 @@ void init_struct(t_game *game)
 	game->frames = &frames;
 //	ft_yonah(game);
 }
-
-
-
-/*
-
-	void	*mlx;
-	void	*img;
-	char	*relative_path = "./test.xpm";
-	int		img_width;
-	int		img_height;
-
-	mlx = mlx_init();
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-*/
